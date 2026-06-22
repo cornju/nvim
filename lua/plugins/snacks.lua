4dfb166d36e12cfda6dcdbdaf45a21aa5f1e7718
cornju <motokih0712@gmail.com>
2026-06-22 13:30:27 +0900

    terminalが開かない問題の修正

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    terminal = {
+      win = {
+        keys = {
+          hide_slash = false,
+          hide_underscore = false,
+        },
+      },
+    },
+  },
+}
